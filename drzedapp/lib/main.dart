5ee4a7eddd3070efee0e62609136deccd4d16c17
Jaian25 <jaiancsedu25@gmail.com>
2025-02-21 15:28:00 +0600

    FIrebase integrated

--- drzedapp/lib/main.dart
+++ drzedapp/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -31,7 +34,10 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Firebase Auth')),
+        body: Center(child: Text('Welcome to Flutter Firebase')),
+      ),
     );
   }
 }
