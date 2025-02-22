6869e951416e34c6159c2acc742f41d704dadcfb
DESKTOP-P2HTOSB\USER <sumaiyatabassum.sts@gmail.com>
2025-02-22 19:55:02 +0600

    Splash Screen add

---
+++ drzedapp/lib/views/splash_screen.dart
@@ -0,0 +1,26 @@
+import 'package:drzedapp/views/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:lottie/lottie.dart';
+class SplashScreen extends StatelessWidget{
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: Center(
+        child: Column(
+          children: [
+            Expanded(child: Lottie.asset('assets/animations/Animation - 1740229235392.json')),
+            Expanded(child: Lottie.asset('assets/animations/Animation - 1740229199714.json')),
+          ],
+        ),
+      ), 
+      nextScreen: const MyHomePage(title: "Dr Zed"),
+      duration: 5000,
+      backgroundColor: Colors.white70,
+      splashIconSize: 1000,
+      );
+  }
+  
+}
